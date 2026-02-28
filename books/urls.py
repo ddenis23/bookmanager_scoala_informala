864b4ef8810aa30cf0268df2d8df91f3ad85bd18
ddenis23 <demian.den245@gmail.com>
2026-02-28 10:00:08 +0200

    changes

--- books/urls.py
+++ books/urls.py
@@ -1,4 +1,11 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [path('', views.hello_world, name='hello_world')]
+urlpatterns = [
+    path('', views.book_list, name="book_list"),
+    path('create_book/', views.create_book, name="create_book"),
+    path('book/<int:pk>/delete/', views.delete_book, name="delete_book"),
+    path('hello/', views.hello_world, name="hello_world"),
+    path('api_simple_endpoint/', views.simple_endpoint, name='simple_endpoint'),
+]
+
--- books/views.py
+++ books/views.py
@@ -1,6 +1,42 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
+
+from books.forms import BookForm
+from books.models import Book
+from django.http.request import HttpRequest
 # Create your views here.
 
-def hello_world(request):
-    return HttpResponse("Hello World, this is my web page")
+def hello_world(request: HttpRequest):
+    return render(request, "books/home.html")
+def book_list(request: HttpRequest):
+    books = Book.objects.all()
+    context = {
+        'books': books
+    }
+    return render(request, "books/home.html", context)
+def create_book(request: HttpRequest):
+    if request.method == "POST":
+        # primim HTTP POST request cand se apasa pe butonul Save la create book.
+        form = BookForm(request.POST)
+        if form.is_valid():
+            # aici se intampla salvarea in baza de date
+            form.save()
+            return redirect("book_list")
+    else:
+        form = BookForm()
+
+    context = {
+        'form': form
+    }
+    return render(request, "books/book_form.html", context)
+def delete_book(request: HttpRequest, pk: int):
+    book = get_object_or_404(Book, pk=pk)
+    if request.method == "POST":
+        book.delete()
+        return redirect("book_list")
+    else:
+        return render(request, "books/book_confirm_delete.html", {"book": book})
+
+
+def simple_endpoint(request: HttpRequest):
+    return HttpResponse("{'content': 'Hello World, this is my response to your request'}", status=202)
